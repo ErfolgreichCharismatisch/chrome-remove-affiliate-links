98a559dedbd273e8fd5a7f874deab0edb2c00c1f
Erfolgreich charismatisch <ivht5bysnmyytii@yahoo.de>
2022-11-17 20:05:18 +0100

    Update backgroundscript.js

--- backgroundscript.js
+++ backgroundscript.js
@@ -1,4 +1,4 @@
-function remove_affiliate_links( details ) {
+function remove_amazon_affiliate_links( details ) {
 	if ( details.url.match( /&tag=[a-zA-Z0-9\-]*/ ) != null) {
 		return {
 			redirectUrl: details.url.replace( /(&tag=[a-zA-Z0-9\-]*)/, '' )
@@ -6,4 +6,19 @@ function remove_affiliate_links( details ) {
 	}
 }
 
-chrome.webRequest.onBeforeRequest.addListener( remove_affiliate_links, { urls: ["*://www.amazon.com/*", "*://www.amazon.co.uk/*", "*://amazon.com/*", "*://amazon.co.uk/*"] }, ["blocking"] );
+function remove_aliexpress_affiliate_links( details ) {
+    if ( details.url.match( /&v=[a-zA-Z0-9\-]*/ ) != null) {
+		return {
+			redirectUrl: details.url.replace( /(&v=[a-zA-Z0-9\-]*)/, '' )
+		};
+	}
+	if ( details.url.match( /spm=[a-zA-Z0-9\-\.]*/ ) != null) {
+		return {
+			redirectUrl: details.url.replace( /(spm=[a-zA-Z0-9\-\.]*)/, '' )
+		};
+	}
+}
+
+chrome.webRequest.onBeforeRequest.addListener( remove_aliexpress_affiliate_links, { urls: ["*://de.aliexpress.com/*", "*://aliexpress.com/*"] }, ["blocking"] );
+
+chrome.webRequest.onBeforeRequest.addListener( remove_amazon_affiliate_links, { urls: ["*://www.amazon.com/*", "*://www.amazon.co.uk/*", "*://amazon.com/*", "*://amazon.co.uk/*", "*://amazon.de/*", "*://www.amazon.de/*"] }, ["blocking"] );
